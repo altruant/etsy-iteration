ca53d69f16f45370d82619101fea240050364ac7
altruant <noellehamberis@gmail.com>
2020-09-10 10:12:04 -0400

    final commit, cleaning up log

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
 // 1. Calculating the average price of all Items
 
 //using reduce to first sum the prices
-  let sum = items.reduce(function(total, current){
-    return total + current['price']
-  }, 0);
-  //since the number of items are the only thing we need to
-  // put in the denominator, we can use arr.length
-  let avg = sum / items.length
-  //rounding the average to two decimal points
-  const avgPrice = Math.round(avg * 100)/100;
+let sum = items.reduce(function(total, current){
+  return total + current['price']
+}, 0);
+//since the number of items are the only thing we need to
+// put in the denominator, we can use arr.length
+let avg = sum / items.length
 
-console.log("$" + avgPrice);
+console.log(`The average price is $${avg.toFixed(2)}.`);
 
+console.log("\n")
 // 2. Getting an array of items costing between $14.00 - 18.00USD
 
-  // filtering for items in USD
-  const USD = items.filter(function(item){
-    return item.currency_code === "USD";
-  });
-  //filter out items in the price range
-  const fourteenEighteen = USD.filter(function(item){
-    return item.price >= 14.00 && item.price <= 18.00;
-  });
-  // getting the array of the items found
-  const final = fourteenEighteen.map(function(item){
-    return item;
-  });
+// filtering for items in USD
+const USD = items.filter(function(item){
+  return item.currency_code === "USD";
+});
+//filter out items in the price range
+const fourteenEighteen = USD.filter(function(item){
+  return item.price >= 14.00 && item.price <= 18.00;
+});
+// logging each item found
+const final = fourteenEighteen.forEach(function(item){
+  console.log(`${item.title}`)
+});
 
-  console.log("Items that cost between $14.00 USD and $18.00 USD:", final);
-
+console.log("\n")
 // 3. Find an item with GBP currency code and log its name and price
 
-  // finding the GBP object
-  let found = items.filter(function(item){
-    return item.currency_code === "GBP";
-  });
-  // console.log(found)
-  // printing the found information into the console using a forEach method
-  const GBP = found.forEach(function(item) {
-    console.log(`${item.title} costs £${item.price}`);
-  })
+// finding the GBP object
+let found = items.filter(function(item){
+  return item.currency_code === "GBP";
+});
+// console.log(found)
+// printing the found information into the console using a forEach method
+const GBP = found.forEach(function(item) {
+  console.log(`${item.title} costs £${item.price}`);
+})
 
-
+console.log("\n")
 // 4. Find items which are made of wood
 
 //filtering items made of wood
@@ -54,6 +52,7 @@ const woodLog = woodFind.forEach(function(item){
   console.log(`${item.title} is made of wood.`)
 });
 
+console.log("\n")
 // 5. Find which items are made of eight or more materials
 
 const matFind = items.filter(function(item){
@@ -68,11 +67,11 @@ ${item.materials.map(function(item){
     }).join("\n")
   }`)
 });
-
+console.log("\n")
 // 6.
 
 madeBySeller = items.filter(function(item){
-    return item.who_made === "i_did"
+  return item.who_made === "i_did"
 })
 
-console.log(`${madeBySeller.length} were made by their sellers.`)
+console.log(`${madeBySeller.length} items were made by their sellers.`)
